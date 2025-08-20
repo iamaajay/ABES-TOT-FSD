36e170829ceb2940898202f3515aec081de22dbe
Ajay Kumar <ajay.kumar@abes.ac.in>
2025-08-20 22:24:52 +0530

    ajay

--- javascript/index.js
+++ javascript/index.js
@@ -6,3 +6,22 @@
 // }). listen(8080);
 var a=20;
 console.log(a);
+const b=12;
+console.log(b);
+let x=5;
+console.log(x);
+let w=10;
+let z=20;
+console.log(w+z);
+let s=5;
+let y=10;
+if(a>b){
+    console.log("hello sir");
+
+}
+else{
+    console.log("sorry try again");
+}
+let d=sum(j,k)
+return sum(4,6);
+console.log(d);

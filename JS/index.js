2e378fcbe61c0232baacaaddfa7df1b7f3e4eed3
Ajay Kumar <ajay.kumar@abes.ac.in>
2025-08-20 10:39:38 +0530

    add

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,13 @@
-var a=12;
-console.log("hi ="+a);
+// var a=12;
+// console.log("hi ="+a);
+//var a=13;console.log(typeof(a));
+//const a=10;
+//const a=15;
+//console.log(a);
+let a=32;
+if(a>10)
+{
+    let a=30;
+    console.log("a ="+a);
+}
+console.log(a);

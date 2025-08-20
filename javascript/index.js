ee6439b63c9972bb9e6d570c00ebf4f5d1457f72
Ajay Kumar <ajay.kumar@abes.ac.in>
2025-08-20 22:07:13 +0530

    add js

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+// let http= require('http');
+// http.createServer(function (req, res)
+// {
+//     res.writeHead(200, {'content-type': 'text/plan'});
+//     res.end('hello sir ');
+// }). listen(8080);
+var a=20;
+console.log(a);
